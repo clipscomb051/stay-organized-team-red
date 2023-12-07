d3f3890b5629833ab8c0236011297fa406d3686e
JawuanHRobinson <jawuanh99@gmail.com>
2023-12-07 16:06:50 -0500

    showin-categories

---
+++ copy_new_todo.js
@@ -0,0 +1,39 @@
+//"use strict"
+//window.onload = function (_event){
+   // getUsers()
+     //   .then(populateUserSelect)
+
+    // getCategories()
+
+     //const userSelect = document.getElementById("user-select")
+    // userSelect.onchange = 
+
+    // categorySelect.onchange = 
+//}
+//function getUsers () {
+    //return fetch(`http://localhost:8083/api/users`)
+     //   .then(response => response.json())
+   // }
+        
+
+//function populateUserSelect(users){
+   // let html = ""
+    //for (const currentUser of users) {
+    html += `<option value="${currentUser.id}"${currentUser.id}</option>`
+//}
+        
+    //const userPick = document.getElementById("users-select")
+    //userPick.innerHTML += html
+   // }
+        
+        
+ 
+
+
+
+
+    
+//function getCategories () {
+    //return fetch(`http://localhost:8083/api/categories`)
+        //.then(response => response.json())
+//}//
--- new_todo.js
+++ new_todo.js
@@ -1,22 +1,50 @@
-window.onload = function (_event){
+"use strict"
+window.onload = function (_event) {
     getUsers()
         .then(populateUserSelect)
+        .then(getCategories)
 
-    // getCategories()
+    const userSelect = document.getElementById("user-select")
+    //userSelect.onchange = handleUserSelect
 
-    // const userSelect = document.getElementById("user-select")
-    // userSelect.onchange = 
-
-    // const categorySelect = document.getElementById("category-select")
-    // categorySelect.onchange = 
+    const categorySelect = document.getElementById("category-select")
+    categorySelect.onsubmit = newToDo
 }
-
-function getUsers () {
+function getUsers() {
     return fetch(`http://localhost:8083/api/users`)
         .then(response => response.json())
 }
-    
-function getCategories () {
-    return fetch(`http://localhost:8083/api/categories`)
-        .then(response => response.json())
+
+function populateUserSelect(users) {
+    let html = ""
+    for (const currentUser of users) {
+        html += `<option value="${currentUser.id}">${currentUser.username}</option>`
+    }
+
+    console.log(users)
+    const userPick = document.getElementById("user-select")
+    userPick.innerHTML += html
 }
+
+function getCategories() {
+    let html = ""
+    fetch(`http://localhost:8083/api/categories`)
+        .then(response => response.json())
+        .then(data => {
+            for (const currentCategory of (data)) {
+                html += `<option value="${currentCategory.id}">${currentCategory.name}</option>`
+
+            }
+            const pickCategory = document.getElementById("category-select")
+            pickCategory.innerHTML = html
+        })
+}
+
+
+function newToDo(event){
+    const addToDo = event.target.value
+    let html = ""
+    
+}
+
+
